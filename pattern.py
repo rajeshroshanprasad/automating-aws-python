407dc0b06c456b197414a610faea008e6a32181f
rajeshroshanprasad <rajesh.roshan.etc@gmail.com>
2020-06-25 14:40:00 -0700

    pattern solution

---
+++ pattern.py
@@ -0,0 +1,14 @@
+for i in range(1,5): #this is refrence to row
+    for j in range(i):
+        print('#', end="")
+
+    print()
+
+print()
+print()
+
+for i in range(4,0,-1): #this is refrence to row
+    for j in range(i):
+        print('#', end="")
+
+    print()
